2ace91552f28f5511cf4295af4591fd48f1cd89d
André Sidrim <andrecorreasidrim@gmail.com>
2023-10-23 15:31:18 -0300

    Estilizacao projeto-frases

--- projeto-frases/src/App.tsx
+++ projeto-frases/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Logo from './assets/logo.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<img className='logo' src={Logo} alt='Logo dev frases' />
+
+			<h2 className='title'>Categorias</h2>
+
+			<section className='categories'>
+				<button className='category-button'>motivacao</button>
+				<button className='category-button'>bem estar</button>
+			</section>
+
+			<button className='button-frase'>Gerar frase</button>
+			<p className='texto-frase'>alguma frase</p>
+		</div>
+	);
 }
 
 export default App;
