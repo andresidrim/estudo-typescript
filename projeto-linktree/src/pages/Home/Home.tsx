f0a77e0a620f11338b79e2cb734009feee4d5c49
André Sidrim <andrecorreasidrim@gmail.com>
2023-10-27 01:46:35 -0300

    renderizacao dos links na pagina home

--- projeto-linktree/src/pages/Home/Home.tsx
+++ projeto-linktree/src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Social from '../../components/Social/Social';
 import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa';
+import { LinkProps } from '../../types/LinkProps';
 import { db } from '../../services/firebaseConnection';
 import {
 	getDocs,
@@ -11,9 +12,56 @@ import {
 	getDoc
 } from 'firebase/firestore';
 
+type SocialProps = {
+	facebook: string;
+	instagram: string;
+	youtube: string;
+};
+
 const Home = () => {
-	const [links, setLinks] = useState([]);
-	const [socialLinks, setSocialLinks] = useState();
+	const [links, setLinks] = useState<LinkProps[]>([]);
+	const [socialLinks, setSocialLinks] = useState<SocialProps>();
+
+	const loadLinks = async () => {
+		const linksRef = collection(db, 'links');
+		const queryRef = query(linksRef, orderBy('created', 'asc'));
+
+		const snapshot = await getDocs(queryRef);
+		let list: LinkProps[] = [];
+
+		snapshot.forEach((doc) => {
+			list.push({
+				id: doc.id,
+				name: doc.data().name,
+				url: doc.data().url,
+				bg: doc.data().bg,
+				color: doc.data().color
+			});
+		});
+
+		setLinks(list);
+	};
+
+	useEffect(() => {
+		loadLinks();
+	}, []);
+
+	const loadSocialLinks = async () => {
+		const docRef = doc(db, 'social', 'link');
+		const snapshot = await getDoc(docRef);
+
+		if (snapshot.data() !== undefined) {
+			setSocialLinks({
+				facebook: snapshot.data()?.facebook,
+				instagram: snapshot.data()?.instagram,
+				youtube: snapshot.data()?.youtube
+			});
+		}
+	};
+
+	useEffect(() => {
+		loadSocialLinks();
+	}, []);
 
 	return (
 		<div className='flex flex-col w-full py-4 items-center justify-center'>
@@ -23,24 +71,39 @@ const Home = () => {
 			<span className='text-gray-50 mb-5 mt-3'>Veja meus links 👇</span>
 
 			<main className='flex flex-col w-11/12 max-w-xl text-center'>
-				<section className='bg-white mb-4 w-full py-2 rounded-lg select-none cursor-pointer transition-transform hover:scale-x-105'>
-					<a>
-						<p className='md:text-lg text-base'>Canal no youtube</p>
-					</a>
-				</section>
-				<footer className='flex justify-center gap-3 my-4'>
-					<Social url='https://facebook.com'>
-						<FaFacebook size={35} color='white' />
-					</Social>
+				{links.map((link) => (
+					<section
+						key={link.id}
+						className='mb-4 w-full py-2 rounded-lg font-medium select-none cursor-pointer transition-transform hover:scale-x-105'
+						style={{ backgroundColor: link.bg, color: link.color }}
+					>
+						<a href={link.url} target='_blank'>
+							<p className='md:text-lg text-base'>{link.name}</p>
+						</a>
+					</section>
+				))}
 
-					<Social url='https://youtube.com'>
-						<FaYoutube size={35} color='white' />
-					</Social>
+				{socialLinks && (
+					<footer className='flex justify-center gap-3 my-4'>
+						{socialLinks.facebook.length > 0 && (
+							<Social url={socialLinks.facebook}>
+								<FaFacebook size={35} color='white' />
+							</Social>
+						)}
 
-					<Social url='https://instagram.com'>
-						<FaInstagram size={35} color='white' />
-					</Social>
-				</footer>
+						{socialLinks.youtube.length > 0 && (
+							<Social url={socialLinks.youtube}>
+								<FaYoutube size={35} color='white' />
+							</Social>
+						)}
+
+						{socialLinks.instagram.length > 0 && (
+							<Social url={socialLinks.instagram}>
+								<FaInstagram size={35} color='white' />
+							</Social>
+						)}
+					</footer>
+				)}
 			</main>
 		</div>
 	);
--- projeto-linktree/src/pages/NotFound/NotFound.tsx
+++ projeto-linktree/src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,20 @@
+import { Link } from 'react-router-dom';
+
 const NotFound = () => {
-	return <div>Página nao encontrada</div>;
+	return (
+		<div className='flex flex-col w-full justify-center items-center text-white min-h-screen'>
+			<h1 className='font-bold text-4xl mb-4'>Página não encontrada</h1>
+			<p className='italic text-xl mb-4'>
+				Você caiu em uma página inexistente!
+			</p>
+			<Link
+				to='/'
+				className='bg-gray-50/20 py-1 px-4 rounded-md transition-transform hover:scale-105'
+			>
+				Voltar para home
+			</Link>
+		</div>
+	);
 };
 
 export default NotFound;
